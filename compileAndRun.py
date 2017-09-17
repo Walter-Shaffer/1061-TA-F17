d8a14842d506224c3a699c9b2b4d91af480b34ee
Walter-Shaffer <31996415+Walter-Shaffer@users.noreply.github.com>
2017-09-17 16:58:12 -0400

    Update compileAndRun.py

--- compileAndRun.py
+++ compileAndRun.py
@@ -1,9 +1,10 @@
-#Run to compile and then execute Java programs that do not require any arguments
-#Will save out put from executing to out.txt file in student sub dir
+#Will compile and then execute Java programs that do not require any arguments
+#Will save out put from executing the java class to out.txt file in student sub dir
 #Will then compare out.txt to the expected.txt file 
 #expected.txt needs to be stored in the cur lab dir or one dir higher than the student dirs
 #If out.txt matches the expected out put out.txt will be renamed pass.txt, if does not match out.txt will be renamed fail.txt
 
+#ex. python compileAndRun.py HelloWorld.java
 
 import os, sys
 import filecmp
@@ -15,12 +16,14 @@ for (root, dirnames, filenames) in os.walk(os.getcwd()):
 
 
 base = os.getcwd()
+javaN = sys.argv[1]
+classN = javaN[0:javaN.index('.')]
 
 for d in dirs:
 	path = d + '/'
 	os.chdir(path)
-	os.system('javac ' + sys.argv[1] )
-	os.system('java HelloWorld > out.txt')
+	os.system('javac ' + javaN )
+	os.system('java ' + classN + ' > out.txt')
 	if filecmp.cmp('out.txt', '../expected.txt', shallow=True): 
 		os.system('mv out.txt pass.txt')
 	else:
